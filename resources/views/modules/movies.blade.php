f1607d4dc0017200d0fd70767f4acbcf1241e0f1
Tarlan Nagiev <tarlan.nagiev@mail.ru>
2020-07-31 18:48:57 +0300

    add movies to modules blade list

---
+++ resources/views/modules/movies.blade.php
@@ -0,0 +1,119 @@
+<!--
+@if (null)
+<section id="movies">
+    <div class="section-title text-center">
+        <h2>Сегодня в кино</h2>
+    </div>
+    <div class="slideset">
+
+        <div class="slides-container">
+            <div class="slides-inner">
+                <div class="slide">
+                    <div class="item-wrapper">
+                        <div class="item-background" style="background-image:url(/images/x1000.webp)"></div>
+                        <div class="item-image">
+                            <div class="item-header d-flex justify-content-end">
+                                @include('modules.add-to-wishlist', array('event_id' => $event->id))
+                            </div>
+                        </div>
+                        <div class="item-content">
+                            <h3 class="item-title">KHRUANGBIN</h3>
+                            <div class="item-info d-flex justify-content-between">
+                                <div class="item-date">@include('assets.calendar')<span>c 30 сентября</span></div>
+                                <div class="item-location">@include('assets.location')<span>ГДК «Украина»</span></div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="slide">
+                    <div class="item-wrapper">
+                        <div class="item-background" style="background-image:url(/images/likvidators.webp)"></div>
+                        <div class="item-image">
+                            <div class="item-header d-flex justify-content-end">
+                                @include('modules.add-to-wishlist', array('event_id' => $event->id))
+                            </div>
+                        </div>
+                        <div class="item-content">
+                            <h3 class="item-title">KHRUANGBIN</h3>
+                            <div class="item-info d-flex justify-content-between">
+                                <div class="item-date">@include('assets.calendar')<span>c 30 сентября</span></div>
+                                <div class="item-location">@include('assets.location')<span>ГДК «Украина»</span></div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="slide">
+                    <div class="item-wrapper">
+                        <div class="item-background" style="background-image:url(/images/portie.webp)"></div>
+                        <div class="item-image">
+                            <div class="item-header d-flex justify-content-end">
+                                @include('modules.add-to-wishlist', array('event_id' => $event->id))
+                            </div>
+                        </div>
+                        <div class="item-content">
+                            <h3 class="item-title">KHRUANGBIN</h3>
+                            <div class="item-info d-flex justify-content-between">
+                                <div class="item-date">@include('assets.calendar')<span>c 30 сентября</span></div>
+                                <div class="item-location">@include('assets.location')<span>ГДК «Украина»</span></div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="slide">
+                    <div class="item-wrapper">
+                        <div class="item-background" style="background-image:url(/images/nimani.webp)"></div>
+                        <div class="item-image">
+                            <div class="item-header d-flex justify-content-end">
+                                @include('modules.add-to-wishlist', array('event_id' => $event->id))
+                            </div>
+                        </div>
+                        <div class="item-content">
+                            <h3 class="item-title">KHRUANGBIN</h3>
+                            <div class="item-info d-flex justify-content-between">
+                                <div class="item-date">@include('assets.calendar')<span>c 30 сентября</span></div>
+                                <div class="item-location">@include('assets.location')<span>ГДК «Украина»</span></div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="slide">
+                    <div class="item-wrapper">
+                        <div class="item-background" style="background-image:url(/images/bilal.webp)"></div>
+                        <div class="item-image">
+                            <div class="item-header d-flex justify-content-end">
+                                @include('modules.add-to-wishlist', array('event_id' => $event->id))
+                            </div>
+                        </div>
+                        <div class="item-content">
+                            <h3 class="item-title">KHRUANGBIN</h3>
+                            <div class="item-info d-flex justify-content-between">
+                                <div class="item-date">@include('assets.calendar')<span>c 30 сентября</span></div>
+                                <div class="item-location">@include('assets.location')<span>ГДК «Украина»</span></div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="slide">
+                    <div class="item-wrapper">
+                        <div class="item-background" style="background-image:url(/images/bilal.webp)"></div>
+                        <div class="item-image">
+                            <div class="item-header d-flex justify-content-end">
+                                @include('modules.add-to-wishlist', array('event_id' => $event->id))
+                            </div>
+                        </div>
+                        <div class="item-content">
+                            <h3 class="item-title">KHRUANGBIN</h3>
+                            <div class="item-info d-flex justify-content-between">
+                                <div class="item-date">@include('assets.calendar')<span>c 30 сентября</span></div>
+                                <div class="item-location">@include('assets.location')<span>ГДК «Украина»</span></div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+    </div>
+</section>
+@endif
+-->
